84e1bd39b037bfc520f4490dd36b6d730e903b3a
Jackson <kanneg881@gmail.com>
2018-10-03 14:16:50 +0800

    add: 14-5 example

---
+++ 14-5/src/Inner.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Inner</h1>
+    </div>
+</template>
+
+<style scoped>
+    h1 {
+        color: blue;
+    }
+</style>
---
+++ 14-5/src/Outer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Outer</h1>
+        <Inner></Inner>
+    </div>
+</template>
+
+<script>
+    import Inner from './Inner.vue';
+
+    export default {
+        components: {
+            Inner,
+        },
+    }
+</script>
+
+<style>
+    div {
+        border: 2px solid red;
+    }
+
+    h1 {
+        color: red;
+    }
+</style>
