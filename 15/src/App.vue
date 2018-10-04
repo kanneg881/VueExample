0591b3be82ae00c293733e3a4d923cd3a527961e
Jackson <kanneg881@gmail.com>
2018-10-04 18:46:49 +0800

    add: 15-2 ~ 15-3 example

---
+++ 15/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="app">
+        <div class="menu">
+            <router-link to="/about">About</router-link>
+            <router-link to="/products">Products</router-link>
+        </div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    import VueRouter from 'vue-router';
+    import About from './About.vue';
+    import Products from './Products.vue';
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        },
+        router: new VueRouter({
+            routes: [
+                {
+                    path: '/about',
+                    component: About,
+                },
+                {
+                    path: '/products/:id?',
+                    component: Products,
+                }
+            ],
+        }),
+    };
+</script>
+
+<style>
+</style>
---
+++ 15/src/Products.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Products</h1>
+        <h2>{{id}}</h2>
+        <h2>{{queryId}}</h2>
+    </div>
+</template>
+
+<script>
+    const products = {
+        18: 'Shoes',
+        26: 'T-Shirts',
+        37: 'Pants',
+    };
+
+    export default {
+        computed: {
+            queryId() {
+                return products[this.$route.query.id];
+            },
+            id() {
+                return products[this.$route.params.id];
+            },
+        },
+    };
+</script>
