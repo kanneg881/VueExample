b99906f2862d6ab17703b05ceb3c8696de58836d
Jackson <kanneg881@gmail.com>
2018-10-09 18:10:11 +0800

    add: 16-4 example

--- 16/src/App.vue
+++ 16/src/App.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="app">
-        <h1>{{count}}</h1>
+        <h1>{{storeCount}}</h1>
+        <h1>{{storeCount2}}</h1>
+        <h1>{{totalCount}}</h1>
         <button @click="addCount">Add</button>
     </div>
 </template>
@@ -10,9 +12,23 @@
 
     export default {
         computed: {
-            ...mapState(['count']),
+            // 用法1
+            // ...mapState(['count']),
+
+            // 用法2
+            ...mapState({
+                storeCount: 'count',
+                storeCount2: state => state.count,
+                totalCount(state) {
+                    return this.localCount + state.count;
+                },
+            }),
         },
-        el: '#app',
+        data() {
+            return {
+                localCount: 5,
+            };
+        },
         methods: {
             ...mapMutations(['addCount']),
         },
